d763ce5032b96b172dbe9565751a45a9d70af437
Anoushka Kaur <anoushkakaur@Anoushkas-MacBook-Pro.local>
2025-06-29 00:08:36 +0800

    Update app.js to use deployed backend URL

--- app.py
+++ app.py
@@ -2,9 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-
-# Allow GitHub Pages frontend
-CORS(app, origins=["https://anoushkaur.github.io"])
+CORS(app, origins="https://anoushkaur.github.io")
 
 @app.route('/chat', methods=['POST'])
 def chat():
